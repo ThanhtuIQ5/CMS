c2423b9a8c01c9734cdb9d3ed72892acd8c8d869
ThanhTuIQ5 <thanhtuvpbq@gmail.com>
2025-11-06 16:49:21 +0700

    Clean copy of branch master (history removed)

---
+++ wordpress/wp-content/themes/twentytwenty/footer.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+?>
+<footer id="site-footer" class="header-footer-group">
+
+    <div class="section-inner">
+
+        <div class="footer-credits">
+            <!-- HTML -->
+            <ul class="social-icons-footer">
+                <li class="social-item-footer">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="social-item-footer">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="social-item-footer">
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                </li>
+                <li class="social-item-footer">
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+                <li class="social-item-footer">
+                    <a href="#"><i class="fa fa-envelope"></i></a>
+                </li>
+            </ul>
+
+            <!-- Nhúng Font Awesome (v4.7) -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+            <p style="margin-bottom: 16px;">
+                <span style="text-decoration: underline;">National Transaction Corporation</span> is a Registered MSP/ISO of Elavon, Inc. Georgia
+                [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]
+            </p>
+            <p>
+                <b>© All rights Reserved.</b> Sunlimetech
+            </p>
+        </div><!-- .footer-credits -->
+
+    </div><!-- .section-inner -->
+
+</footer><!-- #site-footer -->
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
